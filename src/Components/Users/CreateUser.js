d1534a3d8e8ba20cc1ce6b4a5b5ca0e80c6315d8
Aleksandr <sasha.glagol.95@yandex.com>
2022-08-28 20:04:19 +0300

    data Input

--- src/Components/Users/CreateUser.js
+++ src/Components/Users/CreateUser.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 
@@ -5,17 +6,29 @@ import styles from './CreateUser.module.css';
 
 const CreateUser = (props) => {
 
+  const [inputName, setInputName] = useState("");
+  const [inputAge, setInputAge] = useState("");
+
   const createUserHandler = (event) => {
     event.preventDefault();
+    console.log(inputName, inputAge);
   }
+
+  const nameChangeHandler = (event) => {
+    setInputName(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setInputAge(event.target.value);
+  };
 
   return (
     <Card className={styles.input} >
       <form onSubmit={createUserHandler} >
         <label htmlFor="name" >Имя</label>
-        <input id="name" type="text" />
+        <input id="name" type="text" onChange={nameChangeHandler} />
         <label htmlFor="age" >Возраст</label>
-        <input id="age" type="number" />
+        <input id="age" type="number" onChange={ageChangeHandler} />
         <Button type="submit">Добавить Пользователя</Button>
       </form>
     </Card>
